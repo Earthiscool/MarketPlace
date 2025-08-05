9390ba49f74b03f2f94769ce6d704d0681f3ec45
Agastya Bhadani <mailtoagastyab@gmail.com>
2025-08-05 00:12:55 -0400

    s

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className="flex border-t justify-betweeb font-medium p-6">
+            <div className="flex items-center gap-2">
+                <p> Fun Road</p>
+            </div>
+            
+        </footer>
+    )}
